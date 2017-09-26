9d803c9587d7991de74b1c34b6ea3d71fa334e74
Lokesh Devnani <contact@lokeshd.com>
2017-09-27 02:22:58 +0530

    Requests Specs + Factories using Faker + Spec Helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,8 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -54,6 +56,9 @@ RSpec.configure do |config|
       example.run
     end
   end
+
+
+  config.include RequestSpecHelper, type: :request
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
